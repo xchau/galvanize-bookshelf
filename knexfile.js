719f895a0fa1358501ec476ceaadeed68b35a033
Minh Chau <xiu.chau@gmail.com>
2017-01-04 14:20:05 -0800

    Create seeds file for books

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/bookshelf_test'
   },
-
-  production: {}
+  production: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_prod'
+  }
 };
