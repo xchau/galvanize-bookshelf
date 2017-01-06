7be7f7e4bad5a5a545d74f49c11687d352dba9c8
Minh Chau <xiu.chau@gmail.com>
2017-01-05 16:32:43 -0800

    Route /books

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,106 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
+const { camelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .orderBy('title')
+    .then((books) => {
+      res.send(camelizeKeys(books));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+
+      res.send(camelizeKeys(book));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.post('/books', (req, res, next) => {
+  knex('books')
+    .insert({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.coverUrl
+    }, '*')
+    .then((books) => {
+      res.send(camelizeKeys(books[0]));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        const err = new Error('Book id does not exist');
+
+        err.status(400);
+
+        throw err;
+      }
+
+      return knex('books')
+        .update({
+          title: req.body.title,
+          author: req.body.author,
+          genre: req.body.genre,
+          description: req.body.description,
+          cover_url: req.body.coverUrl
+        }, '*')
+        .where('id', req.params.id);
+    })
+    .then((books) => {
+      res.send(camelizeKeys(books[0]));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  let book;
+
+  knex('books')
+    .del('*')
+    .where('id', req.params.id)
+    .then((row) => {
+      if (!row.length) {
+        return next();
+      }
+
+      book = row;
+
+      delete book[0].id;
+      res.send(camelizeKeys(book[0]));
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
